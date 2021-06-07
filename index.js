76392467586d05ef1c1925f4e94ddd0402d2338f
sharathguvvala <guvvalasharath@gmail.com>
2021-06-07 13:24:57 +0530

    setup the view engine as ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = process.env.port || 8000
 //use routes
 app.use("/", require('./routes/index'))
 
+//setup view engine
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running server: ${err}`)
