1bfa4b7c7a6992aeac3d1df69d245ef92244cedb
sharathguvvala <guvvalasharath@gmail.com>
2021-06-07 12:53:21 +0530

    added controller and route for home

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 8000
 
+//use routes
+app.use("/", require('./routes/index'))
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running server: ${err}`)
