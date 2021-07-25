04b15c1529c3c7ccc6295b877a0fa492cd6d57b1
sharathguvvala <guvvalasharath@gmail.com>
2021-07-25 12:58:26 +0530

    added auth for posts

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -43,7 +43,7 @@ module.exports.create = function(req,res){
 }
 
 module.exports.createSession = function(req,res){
-    return res.redirect('/users/profile')
+    return res.redirect('/')
 }
 
 module.exports.Signout = function(req,res){
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const passport  = require('passport')
 
 const postController = require('../controllers/postController')
 
-router.post('/create',postController.create)
+router.post('/create',passport.checkAuth,postController.create)
 
 module.exports = router
