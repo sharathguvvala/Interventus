bd8ab99b3f8dc90c407c53b50eeebd0f0f22cbb5
sharathguvvala <guvvalasharath@gmail.com>
2021-07-25 12:42:31 +0530

    post form

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,18 @@
+const Post = require('../models/post')
+const { post } = require('../routes')
+
 module.exports.home = function(req, res){
-    res.render('home.ejs', {title: "Home"})
+    /* Post.find({},function(err,posts){
+        return res.render('home.ejs', {
+            title: "Interventus | Home",
+            posts:posts
+        })
+    }) */
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home.ejs', {
+            title: "Interventus | Home",
+            posts:posts
+        })
+    })
 }
