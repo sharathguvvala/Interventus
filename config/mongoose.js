b1618d4702e495cd45ed4865cac19c15d091c75d
sharathguvvala <guvvalasharath@gmail.com>
2021-08-03 15:15:02 +0530

    delete comments

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/interventus')
+mongoose.connect('mongodb://localhost/interventus',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
 const db = mongoose.connection;
 db.on('error', console.error.bind(console,'error in connecting to db'))
 db.once('open',function(){
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport = require('passport')
 const commentsController = require('../controllers/commentsController')
 
 router.post('/create',passport.checkAuth,commentsController.create)
+router.get('/destroy/:id',passport.checkAuth,commentsController.destroy)
 
 module.exports = router
